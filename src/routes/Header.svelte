d9f295f0730a380e069e0070bcd29c4af9bfebc2
Henrik <henrik.klein12@gmail.com>
2023-11-30 22:19:36 +0100

    -..-

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,17 +1,28 @@
 <script>
 	import { page } from '$app/stores';
+	import image from '$lib/images/linkedin.png';
 </script>
 
 <header class="d-flex justify-content-center py-3">
-
-	<nav>
-		<ul class="nav nav-pills">
-            <li class="nav-item fs-5">
-				<a class="nav-link active" href="/">Home</a>
-			</li>
-			<li class="nav-item fs-5">
-				<a class="nav-link" href="/about">About</a>
-			</li>
-		</ul>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container-fluid">
+			<div class="collapse navbar-collapse w-100" id="navbarText">
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<li class="nav-item fs-5">
+						<a class="nav-link" aria-current="page" href="/">Home</a>
+					</li>
+					<hr />
+					<li class="nav-item fs-5">
+						<a class="nav-link" href="/about">About</a>
+					</li>
+					<hr />
+					<li class="nav-item">
+						<a class="nav-link" target="_blank" href="https://www.linkedin.com/in/henrik-klein-95a1a9a5/">
+							<img src={image} alt="linkedin" width="30" height="25" />
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
 	</nav>
 </header>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,2 @@
-<svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
-</svelte:head>
-
 <div class="text-column">
 </div>
